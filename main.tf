30f2e544d4191ae71ce8fcc25146ae31e59e21f0
Reese Wilson <3253971+figadore@users.noreply.github.com>
2021-09-22 10:56:15 -0700

    don't disable apis or dependent services on destroy
    release #major

--- main.tf
+++ main.tf
@@ -5,12 +5,14 @@ module "domain_project_factory" {
 
   # Max project id is 30 chars, and we are appending a dash and 4 random chars
   # Also trim trailing dash in case truncated string ends with a dash
-  project_id        = trim(substr("${var.name}-${var.env_id}", 0, 25), "-")
-  name              = substr("${var.name} ${var.env_name}", 0, 30)
-  billing_account   = var.billing_account
-  random_project_id = true
-  org_id            = var.org_id
-  folder_id         = var.env_folder_id
-  activate_apis     = var.activate_apis
+  project_id                  = trim(substr("${var.name}-${var.env_id}", 0, 25), "-")
+  name                        = substr("${var.name} ${var.env_name}", 0, 30)
+  billing_account             = var.billing_account
+  random_project_id           = true
+  org_id                      = var.org_id
+  folder_id                   = var.env_folder_id
+  activate_apis               = var.activate_apis
+  disable_services_on_destroy = false
+  disable_dependent_services  = false
 }
 
