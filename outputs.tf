0c6d614b9863e684787bf2852035cdf2b23be5e9
Reese <3253971+figadore@users.noreply.github.com>
2022-02-23 14:27:56 -0800

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
 output "project_id" {
   value = module.domain_project_factory.project_id
 }
+
+# TODO pass all outputs
+output "service_account_email" {
+  value = module.domain_project_factory.service_account_email
+}
