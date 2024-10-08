6265780a083a9d12ec1af69cd89a5ed838537485
Alex Kalliontzis <alkallio@gmail.com>
2024-10-08 17:26:32 +0300

    Allow project id override
    Introduce a new module input, project_id, to explicitly set the project id.
    If not set, the project id will be generated from the (domain) name, env_id, and a random suffix.

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+locals {
+  project_id        = var.project_id != null ? var.project_id : trim(substr("${var.name}-${var.env_id}", 0, 25), "-")
+  random_project_id = var.project_id == null ? true : false
+}
+
 # Provision a project for the domain
 module "domain_project_factory" {
   source  = "terraform-google-modules/project-factory/google"
@@ -5,10 +10,10 @@ module "domain_project_factory" {
 
   # Max project id is 30 chars, and we are appending a dash and 4 random chars
   # Also trim trailing dash in case truncated string ends with a dash
-  project_id                  = trim(substr("${var.name}-${var.env_id}", 0, 25), "-")
+  project_id                  = local.project_id
   name                        = substr("${var.name} ${var.env_name}", 0, 30)
   billing_account             = var.billing_account
-  random_project_id           = true
+  random_project_id           = local.random_project_id
   org_id                      = var.org_id
   folder_id                   = var.env_folder_id
   activate_apis               = var.activate_apis
--- variables.tf
+++ variables.tf
@@ -47,3 +47,9 @@ variable "default_service_account" {
   default     = "deprivilege"
   type        = string
 }
+
+variable "project_id" {
+  description = "Use this to explicitly set the project id. If not set, the project id will be generated from the (domain) name, env_id, and a random suffix."
+  default     = null
+  type        = string
+}
